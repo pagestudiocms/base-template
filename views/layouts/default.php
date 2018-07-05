980c00faa7a2803f6603aef8e0f9278ebac4ebce
cosmomathieu <cosmo@cosmointeractive.co>
2018-07-05 01:09:36 -0400

    Added base files

---
+++ views/layouts/default.php
@@ -0,0 +1,10 @@
+{{ theme:partial name="header" }}
+
+    <!-- cms content injection point -->
+    <main class="content-container" role="main" data-content-field="main-content">
+        <div class="wrapper">
+            {{ content }}
+        </div>
+    </main>
+    
+{{ theme:partial name="footer" }}
---
+++ views/partials/footer.php
@@ -0,0 +1,26 @@
+
+        <footer class="site-footer" id="footer" data-content-field="footer-content" role="footer">
+            <div class="footer-container container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <p class="m-0 text-center">Copyright &copy; {{ globals.site.title }} {{ helper:date format="Y" }}</p>
+                        <div class="made-by">
+                            Powered By <a href="#" target="_blank">PageStudioCMS v1.4.0
+                        </div>
+                    </div>
+                </div>
+            </div><!-- end .footer-container -->
+        </footer>
+
+    </div> <!-- end .site-container -->
+    
+    {{ builder:template_footer }}
+
+<!-- JavaScript
+====================================================== -->        
+    
+    <!-- Core JavaScript Files -->
+    {{ template:footer }}
+
+</body>
+</html>
